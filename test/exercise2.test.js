33ff3ca3f719544e26fadb01bee0778cef7c9032
Walter <wg904093@gmail.com>
2023-12-01 04:30:07 -0300

    completed exercise 2

---
+++ test/exercise2.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const { simplifyPath } = require('../exercise/exercise2')
+
+test('is the expected path', () => {
+  expect(simplifyPath('/x/./y/../../z/')).toBe('/z')
+  expect(simplifyPath('/home//user/log/../')).toBe('/home/user')
+  expect(simplifyPath('/../')).toBe('/')
+})
